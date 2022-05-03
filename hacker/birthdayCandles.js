acbace17b792ce11a249961e4f987324727dfe63
Lisa <lb.broadhead@gmail.com>
2022-05-03 12:25:11 -0500

    first commit

---
+++ hacker/birthdayCandles.js
@@ -0,0 +1,21 @@
+function birthdayCakeCandles(candles) {
+    var tallest = candles[0];
+    var count = 0;
+    
+    for(var i=1;i<=candles.length-1;i++){
+        if(candles[i] > tallest){
+            tallest = candles[i];
+        }
+    }
+    console.log(tallest);
+    for(var j=0; j<= candles.length-1;j++){
+        if(candles[j] === tallest){
+            count++;
+        }
+        continue;
+    }
+    
+    return count;
+}
+
+console.log(birthdayCakeCandles([18, 90, 90, 13, 90, 75, 90, 8, 90, 43]));
