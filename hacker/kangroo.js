a6ad05b63b1c888d963d8f56648186eddc087eb3
Lisa <lb.broadhead@gmail.com>
2022-05-05 11:18:20 -0500

    first commit

--- hacker/kangroo.js
+++ hacker/kangroo.js
@@ -1,6 +1,26 @@
 function kangaroo(x1, v1, x2, v2) {
     // Write your code here
-   
+    21 6 47 3   
+    27 50
+
+    0 3 4 2
+    3 6
+
+    0 2 5 3
+    2 8
+
+    14 4 98 2
+    56  100, 196
+
+    
+
+    for(var i=0; i <= 10000;i++){
+        if(x1+v1 == x2+v2){
+            
+        }
+    }
+
+    
 }
 
 kangaroo();
