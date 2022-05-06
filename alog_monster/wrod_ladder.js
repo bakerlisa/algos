807433f50da58604dc5cabf2e7cc1df2290137dd
Lisa <lb.broadhead@gmail.com>
2022-05-06 13:59:31 -0500

    first commit

--- alog_monster/wrod_ladder.js
+++ alog_monster/wrod_ladder.js
@@ -45,39 +45,73 @@
 // }
 
 /////////
+// function wordLadder(begin, end, wordList) {
+//     var pervious = begin;
+//     var count = 0;
+//     var counter = 0;
+//     var chain = [begin];
+//     var k = 1;
+
+//     const item = new Set(end.split(""));
+
+//     while(counter < wordList.length){
+//         let word = new Set(wordList[counter].split(""));
+//         pervious = pervious.split("");
+
+//         for(var k=1;k<pervious.length;k++){
+//             if(word.has(pervious[k]) ){
+//                 console.log(word);
+//                 console.log(pervious);
+//                 console.log();
+//                 count++;
+//             }
+//         }
+
+//         if(count >= end-1){ 
+//             chain.push(wordList[k]);
+//         }
+//         counter++;
+//         count = 0;
+//         pervious = wordList[k];
+//     }
+//     // console.log(chain);
+//     return chain.length;
+// }
+
+///// THREE
 function wordLadder(begin, end, wordList) {
     var pervious = begin;
-    var count = 0;
-    var counter = 0;
+    var runner = 1;
+    var runnerTwo = 0;
     var chain = [begin];
-    var k = 1;
-
-    const item = new Set(end.split(""));
-
-    while(counter < wordList.length){
-        let word = new Set(wordList[counter].split(""));
-        pervious = pervious.split("");
-
-        for(var k=1;k<pervious.length;k++){
-            if(word.has(pervious[k]) ){
-                console.log(word);
-                console.log(pervious);
-                console.log();
+    var count = 0;
+    
+    while(runnerTwo<wordList.length){
+        let item = new Set(pervious.split(""));
+        let current = wordList[runner].split("");
+        
+        for(var i=0;i<current.length;i++){
+            if(item.has(current[i])){
                 count++;
             }
         }
 
-        if(count >= end-1){ 
-            chain.push(wordList[k]);
+        console.log(count);
+        console.log(pervious);
+        console.log(wordList[x]);
+        
+        if(count === 3){
+            chain.push(wordList[runner]);
+            runner++;
+            pervious = wordList[perviousIterator];
         }
-        counter++;
-        count = 0;
-        pervious = wordList[k];
+        count = 0;   
+        runner++;
+
     }
-    // console.log(chain);
-    return chain.length;
+    console.log(chain);
 }
 
 console.log(wordLadder("cold","warm",["cold", "gold","cord","sold","card","ward","warm","tard"]));
-console.log(wordLadder("fool","sage",["fool", "pool", "poll", "pole", "pale", "sale", "sage"]));
+// console.log(wordLadder("fool","sage",["fool", "pool", "poll", "pole", "pale", "sale", "sage"]));
 
