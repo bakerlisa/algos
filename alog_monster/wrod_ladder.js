29135c1685ad9a59ecdfa88dc05545e9ee0f9bb1
Lisa <lb.broadhead@gmail.com>
2022-05-06 11:59:58 -0500

    first commit

--- alog_monster/wrod_ladder.js
+++ alog_monster/wrod_ladder.js
@@ -1,23 +1,44 @@
-var start = "cold";
-var end = "warm";
-var word_list = ["cold", "gold","cord","sold","card","ward","warm","tard"];
-var chain = ["cold"];
+wordLadder = () =>{
+    var end = "warm";
+    var word_list = ["cold", "gold","cord","sold","card","ward","warm","tard"];
+    var pervious = "cold";
+    var count = 0;
+    var notIn = "";
+    var chain = [];
 
-const item = new Set(end.split(""));
-console.log(item);
+    const item = new Set(end.split(""));
 
-for(var i=0; i<word_list.length;i++){
-    if(word_list[i] === end){
-        chain.push(word_list[i]);
-        break;
-    }else{
-        let word = word_list[i].split("");
-        for(var j=0;j<word.length;j++){
-            if(item.has(word[j])){
-                chain.push(word_list[i]);
-                break;
+    for(var i=1; i < word_list.length;i++){
+        if(word_list[i] === end){
+            break;
+        }else{
+            let word = new Set(word_list[i].split(""));
+            pervious = pervious.split("");
+
+            for(var k=0;k<pervious.length;k++){
+                if(word.has(pervious[k])){
+                    count++;
+                }else{
+                    notIn = pervious[k];
+                }
+            }
+
+            if(count <= 3){
+                console.log(notIn);                
             }
         }
+        pervious = word_list[i];
     }
+    console.log(chain);
+    return chain.length;
 }
-console.log(chain);
+console.log(wordLadder());
+
+
+// for(var j=0;j<word.length;j++){
+//     if(item.has(word[j])){
+//         chain.push(word_list[i]);
+//         break;
+//     }
+//     console.log(word);
+// }
