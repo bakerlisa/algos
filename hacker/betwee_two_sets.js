74c59b7979a703378e85cbd73cdacc22bdb17462
Lisa <lb.broadhead@gmail.com>
2022-05-07 16:15:26 -0500

    first commit

--- hacker/betwee_two_sets.js
+++ hacker/betwee_two_sets.js
@@ -3,11 +3,9 @@ function getTotalX(a, b) {
     var runner = 2;
 
     for(var x=0;x<b.length;x++){
-        while(b[x] / runner >= runner){
+        while(b[x] > runner){
             if(b[x] % runner === 0){
-                if(runner <= a[a.length - 1]){
-                    allFactors.add(runner);
-                }
+                allFactors.add(runner);
             }
             runner++;
         }
@@ -15,14 +13,86 @@ function getTotalX(a, b) {
     }
     
     allFactors.forEach(element => {
-        if((element % a[0] === 0 || (element % a[1] === 0)) ){
-        }else{
-            allFactors.delete(element);
-        }
+        // if(element > a[0]){
+        //     if(element % a[0] != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }else{
+        //     if(a[0] % element != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }
+
+        // if(element > a[1]){
+        //     if(element % a[1] != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }else{
+        //     if(a[1] % element != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }
+
+
+        
     });
 
+    console.log();
     console.log(allFactors.size);
+    console.log(allFactors);
     return allFactors.size;
 }
-getTotalX([2, 4],[16,32,96]);
+getTotalX([3, 4],[24,48]);
 
+
+
+
+
+
+
+
+function getTotalX(a, b) {
+    var allFactors = new Set();
+    var runner = 2;
+
+    for(var x=0;x<b.length;x++){
+        while(b[x] > runner){
+            if(b[x] % runner === 0){
+                allFactors.add(runner);
+            }
+            runner++;
+        }
+        runner = 2;
+    }
+    
+    allFactors.forEach(element => {
+        // if(element > a[0]){
+        //     if(element % a[0] != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }else{
+        //     if(a[0] % element != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }
+
+        // if(element > a[1]){
+        //     if(element % a[1] != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }else{
+        //     if(a[1] % element != 0){
+        //         allFactors.delete(element);
+        //     }
+        // }
+
+
+        
+    });
+
+    console.log();
+    console.log(allFactors.size);
+    console.log(allFactors);
+    return allFactors.size;
+}
+getTotalX([3, 4],[24,48]);
