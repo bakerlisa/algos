5a98181338078b5349f481d330412adb72328423
Lisa <lb.broadhead@gmail.com>
2022-05-09 10:04:15 -0500

    first commit

--- hacker/migitory_birds.js
+++ hacker/migitory_birds.js
@@ -1,6 +1,31 @@
 function migratoryBirds(arr) {
     // Write your code here
+    var items = {};
+    
 
+
+    for(var i=0; i<arr.length;i++){
+        if(items[arr[i]]){
+            items[arr[i]] = items[arr[i]]+ 1;
+        }else{
+            items[arr[i]] = 1;
+        }
+    }
+    var mostSeen = 0;
+    var counter = 0;
+    for (const item in items) {
+        var vals = Object.values(items);
+        if(vals[counter] > mostSeen){
+            mostSeen=item;
+        }else if(vals[counter] === mostSeen){
+            if(item < mostSeen){
+                mostSeen=item;
+            }
+        }
+        counter++;
+    }
+    // console.log( mostSeen)
+    return mostSeen;
 }
 
-console.log(migratoryBirds());
+console.log(migratoryBirds([1,4,4,4,5,3]));
