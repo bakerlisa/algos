c864a8ab0336cff91bc52f8cc4476db6a968c5c2
Lisa <lb.broadhead@gmail.com>
2022-05-09 08:47:22 -0500

    first commit

--- hacker/divisible_sum_pairs.js
+++ hacker/divisible_sum_pairs.js
@@ -1,6 +1,19 @@
 function divisibleSumPairs(n, k, ar) {
     // Write your code here
-
+    var per = ar[0];
+    var add = 0;
+    var total = 0;
+    
+    for(var i=1; i< ar.length;i++){
+        console.log(ar[i] + ar[per]);
+        if((ar[i] + ar[per]) % k === 0){
+            total++;
+        }
+    
+        per = i;
+    }
+    
+    return total;
 }
 
-console.log(divisibleSumPairs());
+console.log(divisibleSumPairs(6,3,[1, 3, 2, 6, 1, 2]));
