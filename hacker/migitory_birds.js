a073cda0336c57bfcbccd3df74f989f4f315dff8
Lisa <lb.broadhead@gmail.com>
2022-05-09 10:15:12 -0500

    first commit

--- hacker/migitory_birds.js
+++ hacker/migitory_birds.js
@@ -1,8 +1,7 @@
 function migratoryBirds(arr) {
     // Write your code here
+    console.log(arr);
     var items = {};
-    
-
 
     for(var i=0; i<arr.length;i++){
         if(items[arr[i]]){
@@ -12,20 +11,34 @@ function migratoryBirds(arr) {
         }
     }
     var mostSeen = 0;
-    var counter = 0;
+    var mostItem;
+    var counter = 1;
+    var per = 0;
+    var perItm;
+    var vals = Object.values(items);
+
+    console.log(items)
     for (const item in items) {
-        var vals = Object.values(items);
-        if(vals[counter] > mostSeen){
-            mostSeen=item;
-        }else if(vals[counter] === mostSeen){
-            if(item < mostSeen){
-                mostSeen=item;
+        if(vals[counter] < vals[per]){
+            mostSeen = item;
+            console.log(vals[counter]);
+            console.log(vals[per]);
+            if(vals[counter] === vals[per]){
+                if(perItm < item){
+                    mostItem = item;
+                }else{
+                    mostItem = perItm
+                }
             }
-        }
+        } 
+        perItm = item;
+        per = counter;
         counter++;
     }
+
+ 
     // console.log( mostSeen)
     return mostSeen;
 }
 
-console.log(migratoryBirds([1,4,4,4,5,3]));
+console.log(migratoryBirds([1,1,1,4,4,4,5,3]));
