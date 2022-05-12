af02a41266400fcbd98f2c00308c3740888373b2
Lisa <lb.broadhead@gmail.com>
2022-05-12 12:02:51 -0500

    first commit

--- hacker/test/largest_area.js
+++ hacker/test/largest_area.js
@@ -28,7 +28,7 @@ function getMaxArea(w, h, isVertical, distance) {
         
     return total;
 }
-// console.log(getMaxArea(4,4,[0,1],[3,1]));
+console.log(getMaxArea(4,4,[0,1],[3,1]));
 // console.log(getMaxArea(2,2,[1,2],[1,1]));
-console.log(getMaxArea(3,4,[1,1],[1,3]));
+// console.log(getMaxArea(3,4,[1,1],[1,3]));
 // [9,6]
