6d2788f1850151a4014d5d48744d7b3c7a8d6832
Lisa <lb.broadhead@gmail.com>
2022-05-13 14:30:40 -0500

    first commit

--- hacker/drawing_book.js
+++ hacker/drawing_book.js
@@ -1,12 +1,13 @@
 function pageCount(n, p) {
     var pagesTurn = 0;
-
-    if( n - 1 === p && (n % 2 != 0)){
+    if(n === 2 && p === 1 ){
+        pagesTurn = 0;
+    }else if( (p % 2 === 0) && n - 1 === p){
+        pagesTurn = 0;
+    }else if( n - 1 === p && (n % 2 != 0)){
         pagesTurn = 0;
     }else if((n % 2 === 0) && (p+1 == n || p +2 === n) ){
         pagesTurn = 1;
-    }else if( (p % 2 === 0) && n - 1 === p){
-        pagesTurn = 0;
     }else{
         if( Math.ceil(n / 2) > p){
             // from front
@@ -34,9 +35,9 @@ function pageCount(n, p) {
 }
 
 
-console.log(pageCount(6,5));
+// console.log(pageCount(6,5));
 // console.log(pageCount(6,2));
 // console.log(pageCount(5,4));
 // console.log(pageCount(25,15));
-// console.log(pageCount(2,1));
+console.log(pageCount(2,1));
 // console.log(pageCount(7,4));x
