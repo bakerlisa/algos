85fab8ffedc58386c0fb18e9347480a4b5926aa9
Lisa <lb.broadhead@gmail.com>
2022-05-14 14:59:57 -0500

    first commit

--- cracking_book/base_case_bild.js
+++ cracking_book/base_case_bild.js
@@ -1,6 +1,20 @@
-base_case_build = () => {
-    var reuslts = "";
+base_case_build = (string) => {
+    const stringArray = string.split("");
+    var results = [];
+
+
+    for(var i=2;i<stringArray.length;i++){
+        if(results.length == 0){
+            results.push(stringArray[0]);
+        }
+        if(results.length == 1){
+            results.push((string[1]+ stringArray[0]));
+            results.push( stringArray[0] + string[1]);
+        }
+    }
+
+
     return results;
 }
 
-console.log(base_case_build());
+console.log(base_case_build("abc"));
