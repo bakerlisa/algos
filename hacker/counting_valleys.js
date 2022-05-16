012e591e2a9c90925fa1f770a53ec3c42b5bfb1a
Lisa <lb.broadhead@gmail.com>
2022-05-16 15:37:33 -0500

    first commit

--- hacker/counting_valleys.js
+++ hacker/counting_valleys.js
@@ -1,33 +1,55 @@
 function countingValleys(steps, path) {
     var travel = 0;
-    var runner = path[0];
     var valley = 0;
+    var up = 0;
+    var down = 0
+    var past = path[0];
+
+    if(path[0] === 'U'){
+        travel++;
+        up++;
+    }else if(path[0] === 'D'){
+        travel--;
+        down--;
+    }
 
 
     // Write your code here
-    for(var i=0;i<steps;i++){
+    for(var i=1;i<steps;i++){
+
         if(path[i] === 'U'){
             travel++;
+            up++;
         }else if(path[i] === 'D'){
             travel--;
+            down--;
         }
-
-        if(runner ===  "U" && path[i] === "D"){
-            if(travel + 1 === 0){
+        // console.log(`past: ${past}`)
+        // console.log(`current: ${path[i]}`)
+        
+        
+        if(past === "U" && path[i] === "D"){
+            travel++;
+            if(travel === 0 && up >= 0 && down < 0){
                 valley++;
             }
+            up=0;
+            down=0;
         }
         
-        runner = path[i];
-        
+        past = path[i];
     }
-    runner = path[i]
 
-    return  valley;
+    return  `valley: ${valley}`;
 }
 
-// console.log(countingValleys(8, "UDDDUDUU"));
+console.log(countingValleys(8, "UDDDUDUU"));
+// 1
+
 // console.log(countingValleys(12, "DD UU DD U D UUU D"));
 // console.log(countingValleys(12, "DDUUDDUDUUUD"));
-console.log(countingValleys(10, "UDUUUDUDDD"));
+// 2
+
+// console.log(countingValleys(10, "UDUUUDUDDD"));
+// 0
 
