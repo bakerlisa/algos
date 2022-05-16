9ec0a6855b9206b098d5225de7b2f88e86946130
Lisa <lb.broadhead@gmail.com>
2022-05-16 15:00:50 -0500

    first commit

--- hacker/counting_valleys.js
+++ hacker/counting_valleys.js
@@ -1,26 +1,31 @@
 function countingValleys(steps, path) {
-    var up = 0;
-    var down = 0;
-    var total = 0;
+    var travel = 0;
+    var runner = path[0];
     var valley = 0;
 
 
     // Write your code here
     for(var i=0;i<steps;i++){
         if(path[i] === 'U'){
-            up++;
-        }else{
-            down--;
+            travel++;
+        }else if(path[i] === 'D'){
+            travel--;
         }
-        console.log(up);
-        console.log(down);
-        console.log(up + down)
+
+        if(runner ===  "U" && path[i] === "D"){
+            if(travel + 1 >= 0){
+                valley++;
+            }
+        }
         
-        console.log()
+        runner = path[i];
+        
     }
+    runner = path[i]
 
     return  valley;
 }
 
 // console.log(countingValleys(8, "UDDDUDUU"));
+// console.log(countingValleys(12, "DD UU DD U D UUU D"));
 console.log(countingValleys(12, "DDUUDDUDUUUD"));
