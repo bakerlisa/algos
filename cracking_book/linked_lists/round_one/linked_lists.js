0b945811783dc924ef5734aea3f7ccbc95f253f2
Lisa <lb.broadhead@gmail.com>
2022-05-17 13:20:02 -0500

    studing linked lists

---
+++ cracking_book/linked_lists/round_one/linked_lists.js
@@ -0,0 +1,82 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class List{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    
+    isEmpty(){
+        if(this.head == null){
+            return "empty"
+        }
+        return "lock and loaded"
+    }
+
+    add(val){
+        var newNode = new Node(val);
+        this.tail = newNode;
+        var runner = this.head;
+
+        if(this.head === null){
+            this.head = newNode;
+        }else{
+            while(runner.next != null){
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+
+    removeFromBack(){
+        var runner = this.head;
+        var pervious = this.head;
+        while(runner.next != null){
+            pervious = runner;
+            runner = runner.next;
+        }
+        this.tail = pervious;
+        pervious.next = null;
+    }
+    
+    showAll(){
+        var runner = this.head;
+    
+        while(runner != null){
+            if(runner === this.head){
+                console.log(`Head: ${runner.value}`);    
+            }else if(runner === this.tail){
+                console.log(`Tail: ${runner.value}`);    
+            }else{
+                console.log(runner.value);
+            }
+            runner = runner.next;
+        }
+    }
+}
+
+var list = new List();
+
+// IS_EMPTY
+// console.log(list.isEmpty());
+
+// ADD
+list.add(10);
+list.add(11);
+list.add(12);
+list.add(13);
+list.add(14);
+// console.log(list)
+
+// REMOVE
+// list.removeFromBack();
+// list.removeFromBack();
+
+// SHOWALL
+console.log(list.showAll())
