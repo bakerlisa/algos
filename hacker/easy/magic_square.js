5c334ec03039832b0e2d8e2254c8b1e5ed171995
Lisa <lb.broadhead@gmail.com>
2022-05-17 14:58:46 -0500

    studing linked lists

--- hacker/easy/magic_square.js
+++ hacker/easy/magic_square.js
@@ -1,27 +1,22 @@
 function formingMagicSquare(s) {
     // Write your code here
     var cost = 0;
-    for(i=0;i<s.length;i++){
+    for(var i=0;i<s.length;i++){
         let indvCost = 0;
         let sm = s[i][0]
         let lg = s[i][0];
-        for(j=0;j<s[i].length;j++){
+
+        // row
+        for(var j=0;j<s[i].length;j++){
             indvCost += s[i][j];
-            if(sm > s[i][j] && s[i][j] != 9){
-                sm = s[i][j]
-            }else if(s[i][j] === 9){
-                // what to do if its a nine
-            }
-            if(lg < s[i][j] && s[i][j] != 1){
-                lg = s[i][j]
-            }else if(s[i][j] === 1){
-                // what to do if its a 1
-            }
         }
-
         var cost = Math.abs(15 - indvCost);
-
         indvCost = 0;
+
+        //columns
+
+        //diagonals
+
     }
 
     return cost;
