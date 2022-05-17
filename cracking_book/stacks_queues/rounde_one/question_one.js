a24338fe8eba41cf63a93047b96ae7ad60969320
Lisa <lb.broadhead@gmail.com>
2022-05-17 11:38:04 -0500

    first commit

---
+++ cracking_book/stacks_queues/rounde_one/question_one.js
@@ -0,0 +1,38 @@
+// 3.1 Describe how you could use a single array to implement 3 stacks
+
+// 1 array to hold the stack
+// 2 to hold the removed items
+// 3 to 
+
+// well I was thiking of that backwards. So just split them up into thirds and add each third to an array
+
+class Node{
+    constructor(){
+        this.head = null;
+        this.next = null;
+    }
+}
+class List{
+    constructor(){
+        this.head = null
+    }
+
+    isEmpty(){
+
+    }
+
+    pop(){
+
+    }
+
+    push(){
+
+    }
+}
+threeInOne = (arr) => {
+    var stackOne;
+    var stackTwo;
+    var stackThree;
+}
+
+threeInOne([1,2,3,4,5,6,7,8,9])
