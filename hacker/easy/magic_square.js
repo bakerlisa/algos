e513daf6881d608457fb86b51599587a4d948921
Lisa <lb.broadhead@gmail.com>
2022-05-17 15:06:02 -0500

    studing linked lists

--- hacker/easy/magic_square.js
+++ hacker/easy/magic_square.js
@@ -1,6 +1,7 @@
 function formingMagicSquare(s) {
     // Write your code here
     var cost = 0;
+
     for(var i=0;i<s.length;i++){
         let indvCost = 0;
         let sm = s[i][0]
@@ -10,19 +11,17 @@ function formingMagicSquare(s) {
         for(var j=0;j<s[i].length;j++){
             indvCost += s[i][j];
         }
-        var cost = Math.abs(15 - indvCost);
+        cost = Math.abs(15 - indvCost);
         indvCost = 0;
 
-        //columns
-        console.log(s[0])
-        var counter = 0;
-        for(var j=0;j<3;j++){
+        //columns - check this before moving on
+        for(var j=0;j<1;j++){
             for(var l=0;l<3;l++){
-                console.log(s[j][l])
+                indvCost += s[l][j]
             }
-            // indvCost += s[counter][j]
+            cost = Math.abs(15 - indvCost);
         }
-        console.log()
+        
 
         //diagonals
 
