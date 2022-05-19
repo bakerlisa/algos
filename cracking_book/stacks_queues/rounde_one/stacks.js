bd48340b7546d90a6705298a069cd171458ceff3
Lisa <lb.broadhead@gmail.com>
2022-05-19 10:45:28 -0500

    first commit

--- cracking_book/stacks_queues/rounde_one/stacks.js
+++ cracking_book/stacks_queues/rounde_one/stacks.js
@@ -11,4 +11,48 @@ class Stack{
         this.head = null;
         this.tail = null;
     }
+
+    isEmpty(){
+        if(this.head === null){
+            return "empty";
+        }else{
+            return "we have a stack!";
+        }
+    } 
+
+    add(val){
+        var newNode = new Node(val)
+        if(this.head === null){
+            this.head = newNode;
+        }else{
+            var runner = this.head;
+            while(runner.next != null){
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+
+    showAll(){
+        var runner = this.head;
+        if(this.head != null){
+            while(runner != null){
+                if(runner === this.head){
+                    console.log(`Head: ${runner.value}`);
+                }else{
+                    console.log(runner.value)
+                }
+                runner = runner.next
+            }
+        }
+    }
 }
+
+var stack = new Stack();
+stack.add(10)
+stack.add(11)
+stack.add(102)
+
+// stack.showAll();
+
+console.log(stack.isEmpty());
