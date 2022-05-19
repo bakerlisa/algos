16619a885542580ade6b67ba6581ccb9893b0862
Lisa <lb.broadhead@gmail.com>
2022-05-19 10:32:00 -0500

    first commit

---
+++ cracking_book/stacks_queues/rounde_one/stacks.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.next = null;
+        this.pervisou = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+}
