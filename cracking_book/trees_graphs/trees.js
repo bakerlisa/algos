849063fd86f960384b24c126e21ee78f84a1bfe5
Lisa <lb.broadhead@gmail.com>
2022-05-20 10:38:51 -0500

    first commit

---
+++ cracking_book/trees_graphs/trees.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(){
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Node{
+    constructor(){
+        this.head = null;
+    }
+    insert(val){
+        let newNode = new Node(val)
+    }
+}
