85410a09d064dbb7cea27e4f4ad3aaf76188cc87
Lisa <lb.broadhead@gmail.com>
2022-05-26 07:59:09 -0500

    first commit

---
+++ hacker/python/consecutive.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    cons = ""
+    for x in range(0,n+1):
+        cons+=x
+    print(cons)
+    return cons
+
