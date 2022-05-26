3088872fc9577d256887577558f492ddf03b5ee7
Lisa <lb.broadhead@gmail.com>
2022-05-26 15:43:23 -0500

    first commit

--- cracking_book/binary/binary.js
+++ cracking_book/binary/binary.js
@@ -1,10 +1,20 @@
 const binary = (binNum) => {
     const arr = binNum.split("");
+    var runner = 1;
+    total = 0;
+    
     // arr.forEach((i) => {
     //     console.log(binNum[i])
     // })
 
-    
+    for(i=binNum.length - 1;i >= 0; i--){
+        runner * arr[i]
+        total += (runner * arr[i])
+        runner *= 2;
+    }
+    return total;
 }
 
 console.log(binary('0101'))
+// console.log(binary('1010'))
+// console.log(binary('01010101'))
