17c0115c96a6a6eaba9131f39db8dfd55c0bea4f
Lisa <lb.broadhead@gmail.com>
2022-05-27 11:48:48 -0500

    first commit

--- cracking_book/binary/binary.js
+++ cracking_book/binary/binary.js
@@ -11,9 +11,35 @@ const binary = (binNum) => {
     return total;
 }
 
-console.log(binary('0101'))
+console.log(binary('010001'))
 // console.log(binary('1010'))
 // console.log(binary('01010101'))
 
 
 // Tomorrow do the reverse. So take a number and put it into binary
+const reverseBinary = (val) => {
+    var start = 1;
+    var binNum = "";
+    
+    // gets our starting point
+    while(start * 1 < val){
+        start *= 2;
+    }
+
+    // builds out binary number
+    while(val >= 1){
+        if(start < val){
+            val -= start;
+            binNum += "1";
+        }else{
+            binNum += "0";
+        }
+        start /= 2;
+    }
+
+    
+    console.log(val)
+    return binNum;
+}
+
+console.log(reverseBinary(9))
