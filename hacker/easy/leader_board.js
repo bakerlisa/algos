8b5323775d0cf9bf049078f4024b2369ab83a939
Lisa <lb.broadhead@gmail.com>
2022-05-28 18:10:41 -0500

    first commit

---
+++ hacker/easy/leader_board.js
@@ -0,0 +1,7 @@
+function climbingLeaderboard(ranked, player) {
+    // Write your code here
+    console.log()
+}
+
+
+console.log(climbingLeaderboard([100, 100, 50, 40, 40,  20, 10],[ 5, 25, 50, 120 ]));
