95eb4e3f4e19328e3df522362dd178ae350c7afd
Lisa <lb.broadhead@gmail.com>
2022-05-31 09:55:09 -0500

    selection sort

--- cracking_book/sorting_searching/merge_sort.js
+++ cracking_book/sorting_searching/merge_sort.js
@@ -1,6 +1,13 @@
 const mergeSort = (arr) => {
-    console.log(arr)
+    let half = Math.ceil(arr.length / 2)
+    let arr1 = arr.slice(0,half)
+    let arr2 = arr.slice(half, arr.length)
+    
+    console.log(arr1)
+    console.log(arr2)
 }
 
+
+
 // console.log(mergeSort([81,21,3],[23,45,12]))
-mergeSort([81,21,3,23,45,12])
+mergeSort([81,21,3,23,45,12,0])
