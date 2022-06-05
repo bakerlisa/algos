8df952ef8fb6608a80c937500e81fe299b10167e
Lisa <lb.broadhead@gmail.com>
2022-06-04 19:16:39 -0500

    first commit

--- hacker/easy/picking_number.js
+++ hacker/easy/picking_number.js
@@ -1,4 +1,18 @@
 function pickingNumbers(a) {
-    // Write your code here
+    var total = [];
+    var runner = 0;
+    for(var i=1;i<a.length;i++){
+        if((Math.abs(a[i] - a[runner]) <= 1) ){
+            total.push(a[runner])
+        }
+        runner = i;
+    }
+
+    console.log(a)
+    console.log(total.length)
 
 }
+
+console.log(pickingNumbers([ 1, 2, 2, 3, 1, 2 ]))
+// console.log(pickingNumbers([ 4, 6, 5, 3, 3, 1 ]))
+// pickingNumbers([ 4, 6, 5, 3, 3, 1 ])
