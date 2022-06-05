96c412cbf8ee70a1d5a31a0e9d3558e18b260d27
Lisa <lb.broadhead@gmail.com>
2022-06-04 19:20:57 -0500

    first commit

--- hacker/easy/picking_number.js
+++ hacker/easy/picking_number.js
@@ -1,18 +1,37 @@
+// function pickingNumbers(a) {
+//     var total = [];
+//     var runner = 0;
+//     for(var i=1;i<a.length;i++){
+//         if((Math.abs(a[i] - a[runner]) <= 1) ){
+//             total.push(a[runner])
+//         }
+//         runner = i;
+//     }
+
+//     console.log(a)
+//     console.log(total.length)
+
+// }
+
+// console.log(pickingNumbers([ 1, 2, 2, 3, 1, 2 ]))
+// console.log(pickingNumbers([ 4, 6, 5, 3, 3, 1 ]))
+// pickingNumbers([ 4, 6, 5, 3, 3, 1 ])
+
 function pickingNumbers(a) {
-    var total = [];
-    var runner = 0;
+    var total = 0;
+    var returnTotal = 0;
+
     for(var i=1;i<a.length;i++){
-        if((Math.abs(a[i] - a[runner]) <= 1) ){
-            total.push(a[runner])
-        }
-        runner = i;
+        total += a[i]
     }
 
-    console.log(a)
-    console.log(total.length)
-
+    var middle = Math.round(total / a.length);
+    for(var j=0; j<a.length;j++){
+        if(Math.abs(a[j] - middle) <= 1){
+            returnTotal++;
+        }
+    }
+    console.log(returnTotal)
 }
 
 console.log(pickingNumbers([ 1, 2, 2, 3, 1, 2 ]))
-// console.log(pickingNumbers([ 4, 6, 5, 3, 3, 1 ]))
-// pickingNumbers([ 4, 6, 5, 3, 3, 1 ])
