04f6efefd67e1e1aca0bf7749eea65c586a0601d
Lisa <lb.broadhead@gmail.com>
2022-06-10 17:14:44 -0500

    first commit

--- hacker/easy/designer_pdf_viewer.js
+++ hacker/easy/designer_pdf_viewer.js
@@ -1,7 +1,16 @@
 function designerPdfViewer(h, word) {
-    // Write your code here
+    word = word.toLowerCase();
+    const alpha = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','v','y','z'])
+    var max = [...alpha].indexOf(word[0]);
+
+    for(var i=0;i<word.length;i++){
+        if([...alpha].indexOf(word[i]) > max){
+            max = [...alpha].indexOf(word[i])
+        }
+    }
+    return word.length * 1 * max;
 
 }
 
-console.log(designerPdfViewer());
+console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc'));
 designerPdfViewer();
