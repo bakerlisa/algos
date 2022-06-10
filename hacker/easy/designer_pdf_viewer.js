9d563e3c76f22139bb1a4bd3ea4566fdb6eb1043
Lisa <lb.broadhead@gmail.com>
2022-06-10 17:37:47 -0500

    first commit

--- hacker/easy/designer_pdf_viewer.js
+++ hacker/easy/designer_pdf_viewer.js
@@ -1,16 +1,28 @@
 function designerPdfViewer(h, word) {
     word = word.toLowerCase();
-    const alpha = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','v','y','z'])
-    var max = [...alpha].indexOf(word[0]);
+    const alpha = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
+    var max = 0;
 
     for(var i=0;i<word.length;i++){
-        if([...alpha].indexOf(word[i]) > max){
-            max = [...alpha].indexOf(word[i])
+        let idx = [...alpha].indexOf(word.charAt(i))
+        // console.log(idx)
+        console.log(word.charAt(i))
+        console.log(h[idx])
+        if(h[idx] > max){
+            max = h[idx]
         }
     }
-    return word.length * 1 * max;
-
+    return word.length * max;
 }
 
-console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc'));
-designerPdfViewer();
+// console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc'));
+    //9
+console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], 'zaba'));
+console.log(designerPdfViewer([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z], 'qjhwkcexec'));
+
+// console.log(designerPdfViewer([4, 2, 1, 2, 3, 4, 3, 7, 4, 1, 5, 6, 1, 3, 2, 6, 6, 3, 7, 3, 1, 1, 5, 1, 1, 4], 'qjhwkcexec'));
+    // 70
+// console.log(designerPdfViewer([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z], 'qjhwkcexec'));
+// designerPdfViewer();
+
+
