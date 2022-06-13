f1add2a910423980e3688895fc4bcbe3cc846a3c
Lisa <lb.broadhead@gmail.com>
2022-06-13 16:17:25 -0500

    first commit

--- hacker/easy/angry_professor.js
+++ hacker/easy/angry_professor.js
@@ -3,6 +3,9 @@ function angryProfessor(k, a) {
     
 
     for(const arriavelTime of a){
+        // if(arriavelTime <= 0){
+        //     students++;
+        // }
         students += arriavelTime <= 0;
     }
 
@@ -12,6 +15,6 @@ function angryProfessor(k, a) {
 // console.log(angryProfessor([4,3],[-2,-1,0,2]))
 // console.log(angryProfessor([4,2],[0, -1, 2, 1]))
 console.log(angryProfessor(3,[-1, -3, 4,2]))
-console.log(angryProfessor(2,[0, -1, 2, 1]))
+// console.log(angryProfessor(2,[0, -1, 2, 1]))
 
 
