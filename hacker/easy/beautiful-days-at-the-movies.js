adb44e3a038d8a7cb2d8cd5111be5d5d8b9a31e9
Lisa <lb.broadhead@gmail.com>
2022-06-19 17:24:49 -0500

    first commit

---
+++ hacker/easy/beautiful-days-at-the-movies.js
@@ -0,0 +1,32 @@
+const beautifulDays = (i, j, k) => {
+    // Write your code here
+    i = i.toString();
+    j = j.toString();
+
+    var revi = ""; 
+    var revj = "";
+    var total;
+    for(var l=i.length -1; l >= 0;l--){
+        if(i[l] != 0){
+            revi += i[l]; 
+        }
+    }
+
+    if( Math.abs((revi - j)) % k  === 0 ){
+        total = Math.abs((revi - i)) / k
+    }
+    
+    for(var m=j.length -1; m >= 0;m--){
+        if(j[m] != 0){
+            revj += j[m]; 
+        }
+    }
+    if((revj - j) % k  === 0 ){
+        total = k/revj
+    }
+    return total;
+
+}
+
+console.log(beautifulDays(20,23,6))
+
