5bc8ef789d266f6dc476b69d74a0e5022f8a5e65
Lisa <lb.broadhead@gmail.com>
2022-07-01 13:08:31 -0500

    first commit

--- practice/reference_value.js
+++ practice/reference_value.js
@@ -1,17 +1,18 @@
-let a = 10
-let b = "Hi"
-var c = [1,2]
-var d = [1,2]
-// d.push(3)
-// d = [3,4,5]
+// ================= Reference vs. Value =================
+// let a = 10
+// let b = "Hi"
+// var c = [1,2]
+// var d = [1,2]
+// // d.push(3)
+// // d = [3,4,5]
 
 
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
-console.log(c === d)
-console.log(c == d)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(c === d)
+// console.log(c == d)
 // arrays, objects and stored with a memeory location 
 // c only changes if we change whats at the memory location
 
@@ -34,3 +35,21 @@ console.log(c == d)
 // False
 // False
 // Because its comparing memory locations
+
+
+// ================= Works in Functions =================
+
+
+let c = [1,2]
+console.log(c)
+add(c,3)
+console.log(c)
+
+function add (arr, ele){ // arr -> is the memory address
+    // arr.push(ele)
+    array = arr.push(ele)
+    console.log(array)
+}
+
+// primivtive: bool,int,strings,null, ect are passed by values
+// obj, arr, classes passed by reference (memory)
