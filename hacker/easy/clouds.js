3881e7c8befed0e7ec225211bee0e6b789714f73
Lisa <lb.broadhead@gmail.com>
2022-07-08 14:30:03 -0500

    first commit

---
+++ hacker/easy/clouds.js
@@ -0,0 +1,10 @@
+function jumpingOnClouds(c, k) {
+    let newC = c.split(",");
+    for(let i=0; i<k.length;i++){
+        
+    }
+
+
+}
+
+console.log(jumpingOnClouds('8,2',[0, 0, 1, 0, 0, 1, 1, 0]))
